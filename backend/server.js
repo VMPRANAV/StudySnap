a4633e0b4dd9fe5d9331e6acd5343fe1904b1ee9
PRANAV V M <mailidofpranav@gmail.com>
2026-05-02 06:56:45 +0530

    Fixed Strict Origin

--- backend/server.js
+++ backend/server.js
@@ -11,17 +11,17 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5001;
 app.use(cors(
-  {
-    origin: [
-    'http://localhost:5173',
-    'http://localhost:3000',
-    'https://study-snap-one.vercel.app',
-    'https://*.vercel.app'              
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-  }
+  // {
+  //   origin: [
+  //   'http://localhost:5173',
+  //   'http://localhost:3000',
+  //   'https://study-snap-one.vercel.app',
+  //   'https://*.vercel.app'              
+  // ],
+  // methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  // allowedHeaders: ['Content-Type', 'Authorization'],
+  // credentials: true
+  // }
 ));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
